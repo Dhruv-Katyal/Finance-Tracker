cbbd641abde56e597897eb01c71b3fce8a471084
Dhruv-Katyal <dhruvkatyal3038@gmail.com>
2025-08-09 22:50:57 +0530

    Updated feature X or fixed bug Y

--- finance/financetrack/models.py
+++ finance/financetrack/models.py
@@ -2,17 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Category(models.Model):
-    CATEGORY_TYPES = [
-        ('Income', 'Income'),
-        ('Expense', 'Expense'),
-    ]
-    name = models.CharField(max_length=50)
-    category_type = models.CharField(max_length=7, choices=CATEGORY_TYPES)
-
-    def __str__(self):
-        return self.name
-    
 class Transaction(models.Model):
     TRANSACTION_TYPES=[
         ('Income','Income'),
@@ -22,7 +11,6 @@ class Transaction(models.Model):
     title = models.CharField(max_length=255)
     amount = models.DecimalField( max_digits=10, decimal_places=3)
     transaction_type = models.CharField(max_length=10, choices= TRANSACTION_TYPES)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     date = models.DateTimeField()
     
     def __str__(self):
