8f6a3fefccb56a9c72d2bf30fbeab596bc755674
Dhruv-Katyal <dhruvkatyal3038@gmail.com>
2025-08-10 00:27:11 +0530

    Updated feature X or fixed bug Y

--- finance/finance/settings.py
+++ finance/finance/settings.py
@@ -115,7 +115,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIXFILES_DIRS =[
+    BASE_DIR.joinpath('static')
+]
+
+STATIC_ROOT = '/home/dhruv/finance/staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
